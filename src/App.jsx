e99d40176dee820e85b284ab36334c2955e16104
Michael Urich <urich.michael@outlook.com>
2022-10-31 18:48:06 +0100

    fix: BookList works

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,48 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
    const [bookCount, setBookCount] = useState(0)
+   const [bookStore, setBookStore] = useState(['tefdasst'])
+   const input = useRef('')
+
+   const handleSubmit = event => {
+      event.preventDefault()
+      //console.log('clicked')
+      const newBookStore = [...bookStore, input.current.value]
+      setBookStore(prev => newBookStore)
+   }
 
    return (
       <div className='App'>
-         <h1>This is my Book List</h1>
+         <h1>
+            {`This is my Book List with 
+              ${bookStore?.length > 0 ? bookStore.length : 0} books`}
+         </h1>
 
-         <form>
+         <form onSubmit={handleSubmit}>
             <label htmlFor='bookName'></label>
-            <input type='text' id='bookName' placeholder='Type in the book you want to store'/>
+            <input
+               ref={input}
+               type='text'
+               id='bookName'
+               placeholder='Type in the book you want to store'
+            />
+            <button type='submit'>Add to list</button>
          </form>
 
-         <div className='card'>
-            <button onClick={() => setBookCount(bookCount => bookCount + 1)}>
-               count is {bookCount}
-            </button>
-         </div>
+         <ul>
+            {bookStore.length !== 0 ? (
+               <ul>
+                  {bookStore.map(book => (
+                     <li key={book}>{book}</li>
+                  ))}
+               </ul>
+            ) : (
+               <div>There is currently no book in store!</div>
+            )}
+         </ul>
       </div>
    )
 }
